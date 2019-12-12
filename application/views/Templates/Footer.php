696273fb0afe6baa00f9ca6b64ca4c170e3f0579
HanL444 <hansenl444@gmail.com>
2019-12-12 16:52:51 +0700

    commit lanjutan

--- application/views/Templates/Footer.php
+++ application/views/Templates/Footer.php
@@ -1,53 +1,80 @@
-  <footer class="footer">
-                <div class="container-fluid">
-                    
-                    <p class="copyright pull-right">
-                       <center>
-                       <span>Copyright  &copy; 
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script>
-                                   Poukamistore Indonesia</span>
-                    </p>
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Web Programming UNPAS <?= date('Y'); ?></span>
+                    </div>
                 </div>
             </footer>
-        </div>
-    </div>
-        <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Apakah anda ingin keluar?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
+            <!-- End of Footer -->
+
             </div>
-            <div class="modal-body">Klik "Logout" dibawah jika anda ingin keluar.</div>
-            <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
-            <a class="btn btn-primary" href="Login">Logout</a>
+            <!-- End of Content Wrapper -->
+
             </div>
-        </div>
-        </div>
-    </div>
-    </body>
-    
-<!--   Core JS Files   -->
-<script src="<?= base_url('assets/') ?>js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="<?= base_url('assets/') ?>js/bootstrap.min.js" type="text/javascript"></script>
-<script src="<?= base_url('assets/') ?>js/material.min.js" type="text/javascript"></script>
-<!--  Charts Plugin -->
-<script src="<?= base_url('assets/') ?>js/chartist.min.js"></script>
-<!--  Dynamic Elements plugin -->
-<script src="<?= base_url('assets/') ?>js/arrive.min.js"></script>
-<!--  PerfectScrollbar Library -->
-<script src="<?= base_url('assets/') ?>js/perfect-scrollbar.jquery.min.js"></script>
-<!--  Notifications Plugin    -->
-<script src="<?= base_url('assets/') ?>js/bootstrap-notify.js"></script>
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-<!-- Material Dashboard javascript methods -->
-<script src="<?= base_url('assets/') ?>js/material-dashboard.js?v=1.2.0"></script>
-<!-- Material Dashboard DEMO methods, don't include it in your project! -->
-<script src="<?= base_url('assets/') ?>js/demo.js"></script>
+            <!-- End of Page Wrapper -->
+
+            <!-- Scroll to Top Button-->
+            <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+            </a>
+
+            <!-- Logout Modal-->
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Bootstrap core JavaScript-->
+            <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+            <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+            <!-- Core plugin JavaScript-->
+            <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+            <!-- Custom scripts for all pages-->
+            <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+            <script>
+                $('.custom-file-input').on('change', function() {
+                    let fileName = $(this).val().split('\\').pop();
+                    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+                });
+
+
+
+                $('.form-check-input').on('click', function() {
+                    const menuId = $(this).data('menu');
+                    const roleId = $(this).data('role');
+
+                    $.ajax({
+                        url: "<?= base_url('admin/changeaccess'); ?>",
+                        type: 'post',
+                        data: {
+                            menuId: menuId,
+                            roleId: roleId
+                        },
+                        success: function() {
+                            document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+                        }
+                    });
+
+                });
+            </script>
+
+            </body>
+
+            </html> 
--- application/views/Templates/Header.php
+++ application/views/Templates/Header.php
@@ -1,121 +1,26 @@
-<?php
-    $file = $this->session->userdata('alamat');
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('assets/') ?>img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="<?php echo base_url('assets/') ?>img/favicon.png" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Poukamistore</title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-    
-    <!-- Bootstrap core CSS     -->
-    <link href="<?php echo base_url('assets/') ?>css/bootstrap.min.css" rel="stylesheet" />
-    
-    <!--  Material Dashboard CSS    -->
-    <link href="<?php echo base_url('assets/') ?>css/material-dashboard.css?v=1.2.0" rel="stylesheet" />
-    
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="<?php echo base_url('assets/') ?>css/demo.css" rel="stylesheet" />
-    
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="s tylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?= $title; ?></title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <div class="wrapper">
-        <div class="sidebar" data-color="purple" data-image="<?php echo base_url('assets/') ?>img/sidebar-1.jpg">
-            
-            <!-- Anda dapat mengubah warna dari sidebar menggunakan: data-color="purple | blue | green | orange | red"
+<body id="page-top">
 
-        Anda dapat juga menambahkan gambar menggunakan data-image tag -->
-            <div class="logo">
-                <a href="#" class="simple-text">
-                    Poukamistore
-                </a>
-            </div>
-            <div class="sidebar-wrapper">
-                <ul class="nav">
-                    <li <?php if($file == 'member') {?> class="active" <?php } ?>>
-                        <a href="<?php echo site_url('member'); ?>">
-                            <i class="fa fa-users nav_icon"></i>
-                            <p>Data Member</p>
-                        </a>
-                    </li>
-
-                    <li <?php if($file == 'barang') {?> class="active" <?php } ?>>                    
-                        <a href="<?php echo site_url('barang'); ?>">
-                            <i class="fa fa-book nav_icon"></i>
-                            <p>Data Barang</p>
-                        </a>
-                    </li>
-
-                    <li<?php if($file == 'pengirim') {?> class="active" <?php } ?>>
-                        <a href="<?php echo site_url('pengirim'); ?>">
-                            <i class="fa fa-plane nav_icon"></i>
-                            <p>Pengiriman</p>
-                        </a>
-                    </li>
-
-                    <li <?php if($file == 'lokasi') {?> class="active" <?php } ?>>
-                        <a href="<?php echo site_url('lokasi'); ?>">
-                            <i class="fa fa-road nav_icon"></i>
-                            <p>Lokasi</p>
-                        </a>
-                    </li>
-                    <li <?php if($file == 'pembelian') {?> class="active" <?php } ?>>
-                        <a href="<?php echo site_url('pembelian'); ?>">
-                            <i class="fa fa-dollar nav_icon"></i>
-                            <p>Pembelian</p>
-                        </a>
-                    </li>
-                    <li <?php if($file == 'logout') {?> class="active" <?php } ?>>
-                            <a class="dropdown-item" href="<? base_url('Login');?>" data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Logout
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="main-panel">
-            <nav class="navbar navbar-transparent navbar-absolute">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Hi <?php echo $this->session->userdata('username'); ?>!</a> 
-                    </div>
-                    <div class="collapse navbar-collapse">
-
-                         <ul class="nav navbar-nav navbar-right">
-                           
-                            <li>
-                                <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
-                                    <?php echo tanggal(); ?>                                </a>
-                            </li>
-
-                            <li class="dropdown">
-
-
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-user"></i>
-                                    <p class="hidden-lg hidden-md">Notifications</p>
-                                </a>
-                                    <li>
-                                        <a href="<?php echo site_url('login/logout') ?>">Logout</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-           
-                    </div>
-                </div>
-            </nav>
+    <!-- Page Wrapper -->
+    <div id="wrapper"> 
